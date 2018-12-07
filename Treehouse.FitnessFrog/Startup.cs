9bc0f49ca4f834153e3b7c9172e27e14b6cd376c
Jared Abbadusky <u381522@footlocker.com>
2018-12-07 16:56:01 -0500

    Added Startup file

---
+++ Treehouse.FitnessFrog/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehouse.FitnessFrog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
